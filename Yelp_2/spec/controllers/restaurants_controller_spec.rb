1546208fae16b5cf772d58ce295c0c23c7254295
Oliver <o.lucas@live.com>
2015-05-21 12:42:17 +0100

    test passing, added some html

---
+++ Yelp_2/spec/controllers/restaurants_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe RestaurantsController, type: :controller do
+
+  class RestaurantsController < ApplicationController
+    def index
+      # raise 'Hello from the index action'
+    end
+  end
+
+end
